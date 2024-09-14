52681beef177acbc3850750f1aec011ac2df493a
carlangas <80172390+CarlosMatta@users.noreply.github.com>
2024-09-13 21:03:28 -0600

    13/09/24

--- Pagina/tablas.sql
+++ Pagina/tablas.sql
@@ -6,27 +6,28 @@ CREATE TABLE usuarios (
     nombres VARCHAR(100) NOT NULL,
     apellidos VARCHAR(100) NOT NULL,
     contrasena VARCHAR(255) NOT NULL,
-    correo VARCHAR(100) NOT NULL,
-    UNIQUE (correo)
+    correo VARCHAR(100) NOT NULL, 
+    UNIQUE (correo),
+    INDEX (nombres)
 );
 
 CREATE TABLE publicaciones (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    usuario_id INT,
+    nombre_usuario VARCHAR(100) NOT NULL,
     curso_o_catedratico VARCHAR(255) NOT NULL,
     mensaje TEXT NOT NULL,
     fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (usuario_id) REFERENCES usuarios(id)
+    FOREIGN KEY (nombre_usuario) REFERENCES usuarios(nombres)
 );
 
 CREATE TABLE comentarios (
     id INT AUTO_INCREMENT PRIMARY KEY,
     publicacion_id INT,
-    usuario_id INT,
+    nombre_usuario VARCHAR(100) NOT NULL,
     comentario TEXT NOT NULL,
     fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (publicacion_id) REFERENCES publicaciones(id),
-    FOREIGN KEY (usuario_id) REFERENCES usuarios(id)
+    FOREIGN KEY (nombre_usuario) REFERENCES usuarios(nombres)
 );
 
 CREATE TABLE cursos (
